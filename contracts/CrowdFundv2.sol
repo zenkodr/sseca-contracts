41a2bbaa628517c8dea50000f8ae657a081fc95a
Joji Martinez <73366769+zenkodr@users.noreply.github.com>
2023-12-26 13:37:05 -0800

    Update CrowdFundv2.sol

--- contracts/CrowdFundv2.sol
+++ contracts/CrowdFundv2.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.23;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 /**
